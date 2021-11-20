aef4ef6d4efc2fe9f9e3b4481dd950ee9c8ab20f
Carlos Valverde <studio@carlos-valverde.com>
2021-11-20 16:54:13 +0100

    [ADDED] Bootstrap + Sass. [MODIFIED] React + Views

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,27 +6,12 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
-
-mix.webpackConfig({
-    resolve: {
-        alias: {
-            'react': path.resolve('node_modules/react'),
-            'react-dom': path.resolve('node_modules/react-dom'),
-            'components': path.resolve('resources/js/src/components'),
-            'hooks': path.resolve('resources/js/src/hooks'),
-            'layouts': path.resolve('resources/js/src/layouts'),
-            'pages': path.resolve('resources/js/src/pages'),
-        },
-    },
-});
-
-mix.react('resources/js/app.js', 'public/js').extract(['react', 'react-dom']);
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps()
+    .react();
