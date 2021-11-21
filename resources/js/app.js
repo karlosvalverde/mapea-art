abe55c94c3bba2c5e05735fda3e6e1364c045cae
Carlos Valverde <studio@carlos-valverde.com>
2021-11-21 13:42:44 +0100

    [IMPROVED] React integration in Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
 require('./bootstrap');
 import React from 'react';
+import ReactRenderer from './src/ReactRenderer';
+
+import TestComponent from './src/components/TestComponent';
+
+const components = [
+    {
+        name: "TestComponent",
+        component: <TestComponent/>,
+    },
+];
+
+new ReactRenderer(components).renderAll();
---
+++ resources/js/src/components/TestComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TestComponent(props) {
+    return (
+        <>
+            <h2>Este elemento es un componente de React.</h2>
+        </>
+    )
+}
