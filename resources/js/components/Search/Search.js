a6b43297b70cc28724b879d7daee6f91c7b8b244
Carlos Valverde <studio@carlos-valverde.com>
2021-12-19 23:00:20 +0100

    Minor fix

--- resources/js/components/Search/Search.js
+++ resources/js/components/Search/Search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { searchByName } from "../../api/researchers";
+// import { searchByName } from "../../api/researchers";
 
 export default function Search() {
     // const [query, setQuery] = useState("");
