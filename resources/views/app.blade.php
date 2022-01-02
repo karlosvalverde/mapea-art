878d3a0af4f4f8d14809fc966df8d9f3289937ec
Carlos Valverde <studio@carlos-valverde.com>
2022-01-03 00:04:52 +0100

    Project refactoring + Viewport detection

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>mapea.art</title>
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
